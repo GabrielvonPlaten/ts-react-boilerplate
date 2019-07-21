bf264c355eee02172f8b5eac7bb4540d918374f0
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-07-21 23:23:59 +0200

    Add Prettier with a few airbnb rules

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,18 @@ module.exports = {
   entry: ['babel-polyfill', './src/index.tsx'],
   output: {
     path: path.join(__dirname, 'public', 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         use: ['babel-loader', 'ts-loader'],
         test: /\.ts|\.tsx$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.s?a?c?ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] // multiple loaders using an array
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'], // multiple loaders using an array
       },
       {
         test: /\.(png|jpg|gif|svg)$/i,
@@ -25,29 +25,29 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: undefined
-            }
-          }
-        ]
-      }
-    ]
+              limit: undefined,
+            },
+          },
+        ],
+      },
+    ],
   },
 
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
 
   devtool: 'cheap-module-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     historyApiFallback: true,
-    publicPath: '/dist/'
+    publicPath: '/dist/',
   },
   mode: 'development',
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css'
+      filename: 'style.css',
     }),
-    new Dotenv()
-  ]
+    new Dotenv(),
+  ],
 };
